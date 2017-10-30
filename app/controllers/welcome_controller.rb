b5cd5624634c46f9712e56f5797056e4f0543c39
Katie Keel <akatiekeel@gmail.com>
2017-10-30 15:46:15 -0600

    Removed unnecessary controllers

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def show
-    
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   get "/api/v1/tanks", to: "api/v1/tanks#index"
-  get "/", to: "welcome#show"
 end
