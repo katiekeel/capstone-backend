f256a61d47718fd8c518a6618623aebec8d13aaf
Katie Keel <akatiekeel@gmail.com>
2017-10-30 13:19:37 -0600

    Connecting to MQTT broker correctly and list of fun topics curated

--- app/services/mqtt_service.rb
+++ app/services/mqtt_service.rb
@@ -1,23 +1,22 @@
-# Make this into an object and use it on server startup in initializer
-
 class MqttService
 
   def initialize
     @client = MQTT::Client.connect(
       :host => 'm2m.eclipse.org',
-      :port => 1883
+      :port => 1883,
     )
   end
 
   def connect_to_broker
-    @client.connect() do |c|
-      c.get('test') do |tank, level|
-        puts "#{tank}: #{level}"
-        Tank.update_tank(tank, level)
-        puts Tank.find_by(number: tank).level
-      end
+    @client.connect()
+    subscribe
+  end
+
+  def subscribe
+    # @client.subscribe( '#' )
+    @client.get( 'owntracks/mario_e/marlin' ) do |topic, message|
+      puts "#{topic}: #{message}"
     end
-    @client.disconnect()
   end
 
 end
