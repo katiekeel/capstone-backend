7fbf4c2e63c941badd04583675f5db8cacaaab18
Katie Keel <akatiekeel@gmail.com>
2017-11-05 09:46:06 -0700

    Allowed requests from localhost 3001

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Capstone
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://localhost:3001',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
